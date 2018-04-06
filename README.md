364e5cc6440d229b3f2ee0eb0d706165777a78c6
Jim Palowski <palowskijim@gmail.com>
2018-04-06 16:39:07 -0700

    Add a README.md

---
+++ README.md
@@ -0,0 +1,51 @@
+# Pizza Shop
+
+#### A program that allows users to select certain items for a pizza and order to get the output of a cost.
+
+#### By **Jim Palowski**
+
+## Description
+
+An application that takes a users input of selecting from three forms with the intention of ordering a pizza
+and to see how much the pizza would cost.
+
+
+
+
+
+### Specs
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| **Homepage** | User select from three forms | Homepage with user input form |
+
+
+
+## Setup/Installation Requirements
+
+1. To run this program, you must have a Atom, HTML, CSS, Bootstrap and JS downloaded.
+2. Install Atom, Bootstrap 3.3.7 and JS. Follow the link for installation instructions.
+    Bootstrap - www.bootstrap.com
+    JS - https://code.jquery.com/jquery-3.3.1.js
+3. Clone this repository.
+
+
+## Known Bugs
+* No known bugs at this time.
+
+## Technologies Used
+  * HTML
+  * CSS
+  * JS
+  * Bootstrap
+
+
+
+## Support and contact details
+
+_Jim Palowski for comments, or concerns._
+
+### License
+
+*{This software is licensed under the MIT license}*
+
+Copyright (c) 2018 **_Jim Palowski_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,52 @@
 //business logic
-
+var Pizza = function(size, topping, sauce){
+  this.size = size;
+  this.toppings = topping;
+  this.sauce = sauce;
+}
 
 // user interface logic
 $(document).ready(function() {
 $("#pizzaForm").submit(function(event){
   event.preventDefault();
+  var userSize = $("input:radio[name=pizzaSize]:checked").val();
+  var userToppings = $("input[type=checkbox][name=pizzaToppings]:checked").val();
+  var userSauce  =$("input:radio[name=pizzaSauce]:checked").val();
+  var newPizza = new Pizza(userSize, userToppings, userSauce);
 
+
+
+
+// Calculate prices
+  var pizzaPrice = 8;
+    if (newPizza.size === ("Medium")) {
+      pizzaPrice +=2;
+    } else if (newPizza.size === ("Large")) {
+        pizzaPrice += 4;
+      }else if (newPizza.size === ("Small")) {
+        pizzaPrice += 4;
+      } 
+      else {
+        pizzaPrice;
+      }
+
+    if (newPizza.toppings === ("Cheese") || newPizza.toppings === ("Sausage") || newPizza.toppings === ("Pepperoni")) {
+      pizzaPrice += 2;
+    }  else {
+      pizzaPrice;
+    }
+    if (newPizza.sauce === ("Tomato")) {
+      pizzaPrice +=2;
+    } else if (newPizza.sauce === ("Marinara")) {
+        pizzaPrice += 4;
+      }else if (newPizza.sauce === ("Alfredo")) {
+        pizzaPrice += 4;
+      } 
+      else {
+        pizzaPrice;
+      }
+  
+$(".Results").show();
+$(".pizzaCost").text(pizzaPrice)
 });
 });
